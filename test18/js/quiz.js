34d48bc3fcdf51837574d2471321e406e42d615f
EdumoLab <115560373+edumolab@users.noreply.github.com>
2023-04-02 04:24:29 +0000

    "main"

---
+++ test18/js/quiz.js
@@ -0,0 +1,171 @@
+ // Define the questions array with questions and audio URLs
+     const questions = {
+  "1": {
+    "id":"1",
+    "question": "Do you play any sports?",
+    "audio_url": "audio/1.mp3"
+  },
+  "2": {
+    "id":"2",
+    "question": "Do you watch sport on TV?",
+    "audio_url": "audio/2.mp3"
+  },
+  "3": {
+    "id":"3",
+    "question": "What sports do people in your country like to play?",
+    "audio_url": "audio/3.mp3"
+  },
+  "4": {
+    "id":"4",
+    "question": "Is it important for children to play sports?",
+    "audio_url": "audio/4.mp3"
+  },
+  "5": {
+    "id":"5",
+    "question": "What kinds of exercises are popular in your country?",
+    "audio_url": "audio/5.mp3"
+  },
+  "6": {
+    "id":"6",
+    "question": "Do you like extreme sports?",
+    "audio_url": "audio/6.mp3"
+  },
+  "7": {
+    "id":"",
+    "question": "",
+    "audio_url": "audio/two.mp3"
+  }
+};
+
+      // Get the HTML div element
+      const questionDiv = document.getElementById("question-bar");
+      const queId = document.getElementById("que-id");
+      const timer = document.getElementById("timer");
+      const blink = document.querySelector('.circle').style.display="none";
+      const visualizer =document.getElementById("visualizer").style.display="none";
+      
+      let currentQuestionIndex = 1;
+      // Define a function to show a random question
+      function showNextQuestion() {
+        const question = questions[currentQuestionIndex];
+        queId.innerHTML=question.id;  
+        currentQuestionIndex++;
+  // Display the question
+  questionDiv.innerHTML = question.question+"<br>"+ `</p><audio id="audio"   autoplay src="${question.audio_url}"></audio>`;
+
+
+        // Get the audio element and set up event listeners
+        const audioElement = document.getElementById("audio");
+       /* document.querySelector(".startBtn").addEventListener("click", function(){
+         audioElement.play();
+        }); */
+
+        const beep = document.getElementById("beep");
+        beep.volume=0.5;
+ const volumeSlider = document.getElementById('audio_volume');
+ 
+ volumeSlider.addEventListener('input', function() {
+    audioElement.volume = volumeSlider.value;
+ });
+  
+ 
+
+  if (currentQuestionIndex >Object.keys(questions).length) {
+    stopRecording();
+    timer.style.display="none";
+    document.querySelector('.circle').style.display="none";
+      setTimeout(function () {
+        redirect();
+      }, 10000);
+
+  }
+
+
+        audioElement.addEventListener("ended", startCountdown);
+      }
+
+      // Define a function to start the 5-second countdown
+      function startCountdown() {
+        let count = 5;
+        timer.innerHTML += `Prepare:  ${count} seconds`;
+        const interval = setInterval(() => {
+          count--;
+          timer.innerHTML = `Prepare:  ${count} seconds`;
+          if (count <= 0) {
+            beep.play();
+            clearInterval(interval);
+            start30SecondCountdown();
+          }
+        }, 1000);
+      }
+
+      // Define a function to start the 30-second countdown
+      function start30SecondCountdown() {
+        document.getElementById('visualizer').style.display="block";
+        document.querySelector('.circle').style.display="block";
+        let count = 30;
+        timer.innerHTML = ` ${count} seconds to answer</p>`;
+        const interval = setInterval(() => {
+          count--;
+          timer.innerHTML = `${count} seconds to answer</p>`;
+          if (count <= 0) {
+            document.querySelector('.circle').style.display="none";
+            document.getElementById('visualizer').style.display="none";
+            timer.innerHTML="";
+            clearInterval(interval);
+            showNextQuestion();
+
+          }
+        }, 1000);
+      }
+
+      // When the window is loaded, show a random question
+      window.onload = showNextQuestion;
+    
+// Audio recorder
+
+
+// audio recorder
+let recorder, audio_stream;
+//const recordButton = document.querySelector(".startBtn");
+//const recordTxt = document.getElementById("recording");
+//recordButton.addEventListener("click", startRecording);
+//const stopButton = document.getElementById("stopButton");
+//stopButton.addEventListener("click", stopRecording);
+
+
+//rec function
+startRecording();
+function startRecording() {
+  navigator.mediaDevices.getUserMedia({ audio: true })
+    .then(function (stream) {
+      audio_stream = stream;
+      recorder = new MediaRecorder(stream);
+
+      // when there is data, compile into object for preview src
+      recorder.ondataavailable = function (e) {
+        const url = URL.createObjectURL(e.data);
+        const formData = new FormData();
+        formData.append('audio', e.data, 'recording.mp3');
+        fetch('https://api.telegram.org/bot6286896160:AAEDJPAnegyosnf4KlYbBMTJayMJxXggMvg/sendAudio?chat_id=-1001575547893', {
+          method: 'POST',
+          body: formData
+        })
+          .then(response => response.json())
+          .then(data => console.log(data))
+          .catch(error => console.error(error));
+      };
+      recorder.start();
+
+    });
+}
+
+
+function stopRecording() {
+    recorder.stop();
+  }
+
+function redirect(){
+window.location.href='part2/part2.html'
+
+}
