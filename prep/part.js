5cc5971ab288bacb58f0beaaeb539147168569ae
EdumoLab <115560373+edumolab@users.noreply.github.com>
2023-04-11 09:27:54 +0000

    "main"

--- prep/part.js
+++ prep/part.js
@@ -225,7 +225,7 @@ cue.forEach((item) => {
  liste.classList.add("card")
  liste.classList.add("py-3")
  liste.classList.add("px-3")
- AudioListener.classList.add("my-3")
+ liste.classList.add("my-3")
 
  liste.appendChild(flex)
  list.appendChild(liste);
